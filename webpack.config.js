e67aa6f3a64138cf2d60d2fd2768a3e65abb2042
Alexey Maltsev <aamaltsev@skbkontur.ru>
2017-11-23 19:17:37 +0500

    Implemented updating lib in demo after webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 const path = require('path');
 const webpack = require('webpack');
+const WebpackCopyAfterBuildPlugin = require('webpack-copy-after-build-plugin');
 
 module.exports = {
-  entry: './src/collider.js',
+  entry: {
+    main: './src/collider.js'
+  },
   output: {
     filename: 'collider.js',
     path: path.resolve(__dirname, 'dist')
   },
   plugins:[
+    new WebpackCopyAfterBuildPlugin({
+      main: '../demo/js/collider.js'
+    }),
     new webpack.optimize.UglifyJsPlugin()
   ]
 };
